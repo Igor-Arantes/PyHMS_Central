47513faf0f03096c64a3814e9711bf5c0acc61ce
Igor Arantes <igor.l.arantes@gmail.com>
2021-06-15 13:08:56 -0300

    move branch

--- DataAquisition.py
+++ DataAquisition.py
@@ -3,6 +3,29 @@ import pandas as pd
 
 
 
-Unidade = pd.read_json('http://smp26ocn02:8509/get/data?localtime&airpresok&headvsok&heavprok&heavvlok')
 
-print(Unidade)
+
+
+pre_get = 'http://'
+get_hms = '/get/data?localtime&airpresok&airtempok&headvsok&heavprok&heavvlok&ptchupvok&relhumiok&rollptvok&winddirok&windspdok'
+get_epta = '/get/data?localtime&airpresok&airtempok&headvsok&relhumiok&winddirok&windspdok'
+
+
+def get_status_hms(target, pre_get, get_hms):
+    try:
+        status = pd.read_json(pre_get+target+get_hms,orient='index')
+        status.columns = ['Status']
+        return status
+    except:
+        return print(f"Error: Type {target} not conected")
+    
+    
+
+def get_status_epta(target, pre_get, get_epta):
+    try:
+        status = pd.read_json(pre_get+target+get_epta,orient='index')
+        status.columns = ['Status']
+        return status
+    except:
+        return print(f"Error: Type {target} not conected")
+    
